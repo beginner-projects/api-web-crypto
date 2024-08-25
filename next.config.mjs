b4f79ed765e323dde1956653f71ebcb53ff0ae94
nick <nickj8366@gmail.com>
2024-08-25 13:13:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  webpack(config) {
+    // Ignore 'puppeteer-core' and 'chrome-aws-lambda' modules for Webpack
+    config.externals = ['puppeteer-core', 'chrome-aws-lambda'];
+
+    return config;
+  },
 };
 
 export default nextConfig;
